e3ae910aa663ae5914bdb11bdacab921661bf9ca
Harikiran Madishetti <harikiranmadishetti95@gmail.com>
2021-04-29 18:25:48 -0500

    Added method getRedactedFeatures to construct the features from the redacted names

--- project2/unredactor.py
+++ project2/unredactor.py
@@ -78,3 +78,47 @@ def extractRedacted(path):
     redacted_data.extend(getRedactedFeatures(data))
 
     return redacted_data  # Returning the features of the redacted names
+
+# This method is used to construct the features for the redacted names
+
+
+def getRedactedFeatures(data):
+
+    redacted_names = []  # list of dictionary of constructed features
+    # Using the below regex pattern to identify the redacted names
+    pattern = re.compile('\u2588+\s*\u2588*\s*\u2588*\s*\u2588+')
+
+    # Finding all the names with matching pattern
+    matched_names = re.findall(pattern, data)
+
+    for names in matched_names:
+        features = {}
+
+        # Removing extra spaces present in between words
+        name = re.sub('\s+', ' ',  names)
+
+        features['name_len_s'] = len(name)  # Length of name with spaces
+        # Length of name without spaces
+        features['name_len'] = len(name.replace(' ', ''))
+        # No. of words in a name
+        features['word_cnt'] = len(name.split(' '))
+        # No. of white spaces in a name
+        features['white_space'] = len(name) - len(name.replace(' ', ''))
+        features['w1_len'] = 0  # Length of 1st word
+        features['w2_len'] = 0  # Length of 2nd word
+        features['w3_len'] = 0  # Length of 3rd word
+        features['w4_len'] = 0  # Length of 4th word
+        words = name.split(' ')
+
+        for i in range(len(words)):  # Finding the length of words
+            if i == 0:
+                features['w1_len'] = len(words[i])
+            elif i == 1:
+                features['w2_len'] = len(words[i])
+            elif i == 2:
+                features['w3_len'] = len(words[i])
+            elif i == 3:
+                features['w4_len'] = len(words[i])
+
+        redacted_names.append(features)
+    return redacted_names
